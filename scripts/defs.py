d55742223d56e52de2c8a28d859efa868235ea8f
xdrie <xdrie@alt.icu>
2021-09-09 15:29:53 -0700

    rename out rom

--- scripts/defs.py
+++ scripts/defs.py
@@ -1,2 +1,2 @@
 SOURCE_ROM = "BPRE0.gba"
-ROM_NAME = "test.gba"
+ROM_NAME = "BPRE0_Upgrade"
--- scripts/insert.py
+++ scripts/insert.py
@@ -9,7 +9,7 @@ import _io
 
 OFFSET_TO_PUT = 0x900000
 SOURCE_ROM = "BPRE0.gba"
-ROM_NAME = "test.gba"
+ROM_NAME = "BPRE0_Upgrade.gba"
 
 if sys.platform.startswith('win'):
     PathVar = os.environ.get('Path')
